f35b54a85120cce25ffb982bed4a8fc033f8b72f
Gabriel Lewis <gabrielhlewis@gmail.com>
2018-04-18 10:43:47 -0700

    Initial Commit

--- MetalBreakOut/ViewController.swift
+++ MetalBreakOut/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import MetalKit
+import SceneKit
 
 class ViewController: UIViewController {
 
+    enum BackgroundColor {
+        static let green = MTLClearColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.0)
+    }
+
+    @IBOutlet weak var metalView: MTKView!
+    var device: MTLDevice!
+    var commandQueue: MTLCommandQueue!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        metalView.device = MTLCreateSystemDefaultDevice()
+        device = metalView.device
+        metalView.clearColor = BackgroundColor.green
+        metalView.delegate = self
+        commandQueue = device.makeCommandQueue()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+
+    }
+
+    func draw(in view: MTKView) {
+        guard let drawable = view.currentDrawable, let descriptor = view.currentRenderPassDescriptor else { return }
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: descriptor)
+        commandEncoder?.endEncoding()
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
+    }
+}
