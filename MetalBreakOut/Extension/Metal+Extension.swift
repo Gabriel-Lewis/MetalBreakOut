015fedfb693dc5ed5bfc4b5a739f26cb8df4d13e
Gabriel Lewis <gabrielhlewis@gmail.com>
2018-04-20 15:15:23 -0700

    Adds an extension of MTLClearColor that hold different colors

---
+++ MetalBreakOut/Extension/Metal+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Metal+Extension.swift
+//  MetalBreakOut
+//
+//  Created by Gabriel Lewis on 4/20/18.
+//  Copyright © 2018 Gabriel Lewis. All rights reserved.
+//
+
+import Foundation
+import MetalKit
+
+extension MTLClearColor {
+    static let wenderlichGreen = MTLClearColor(red: 0.0,
+                                               green: 0.4,
+                                               blue: 0.21,
+                                               alpha: 1.0)
+    static let skyBlue = MTLClearColor(red: 0.66,
+                                       green: 0.9,
+                                       blue: 0.96,
+                                       alpha: 1.0)
+}
